30c95eba002708386b657027eaba7ab9baea61c2
Thibaud <thibaud97@hotmail.fr>
2018-03-30 10:39:30 +0200

    Form preuve depense

--- pages/preuve.php
+++ pages/preuve.php
@@ -1,15 +1,23 @@
 <div data-role="page" id="preuve">
     <div role="main" class="ui-content">
-        <h1>Preuve</h1>
+        <form method="POST" action="/preuve">
+            <h2>Rajouter une preuve de dépense: </h2>
+
+            <label for="intitule">Intitulé:</label>
+            <input name="intitule" id="intitule" value="" type="text">
+
+            <label for="date">Date de dépense:</label>
+            <input name="date" id="date" value="" type="date">
+
+            <label for="depense">Dépense:</label>
+            <input name="depense" pattern="[0-9]*" id="depense" type="number" placeholder="Prix en €" >
+        
+            <label for="photo">Preuve photo:</label>
+            <input name="photo" id="photo" value="" type="file">
+
+            <button type="submit">Enregistrer</button>
+        </form>
     </div>
-
-
-
-
-
-
-
-
 
     <?php include('includes/footer.php'); ?>
 </div>
