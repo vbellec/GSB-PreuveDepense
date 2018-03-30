ef5f516bd31f475f7b64b2701ed51bf082906d94
Thibaud <thibaud97@hotmail.fr>
2018-03-30 09:19:11 +0200

    init jquery-mobile

---
+++ includes/footer.php
@@ -0,0 +1,18 @@
+<div data-role="footer" data-theme="b" data-position="fixed" >
+    <div data-role="navbar" data-iconpos="top">
+        <ul>
+            <li>
+                <a href="/" data-icon="home">Home</a>
+            </li>
+
+            <li>
+               <a href="/preuve" data-icon="camera">Preuve</a>
+            </li>
+
+            <li>
+                <a href="/settings" data-icon="gear">Settings</a>
+            </li>
+
+        </ul>
+    </div>
+</div>
---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>GSB - Preuve Dépense</title>
+    <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
+    <link rel="stylesheet" href="/css/style.css" />
+    <script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
+    
+</head>
+<body>
+	
+
+
+
+
+
+
+
+
+
+
+
+
+    <?php include('includes/footer.php'); ?>
+    <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+</body>
+</html>
